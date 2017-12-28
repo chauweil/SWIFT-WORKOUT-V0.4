ee3a0c595630cf6c9dffac4335f8252475399906
Jeff <jeff.chau.qin@gmail.com>
2017-12-28 01:02:29 +0100

    appli recipt'

---
+++ ticket_reader/ticket_reader/README.md
@@ -0,0 +1,19 @@
+#  Receipt scanner & management
+
+### Installation
+
+On peut utiliser la caméra et aux photos. Il faut l'ajouter dans Info.plist et 
+```
+<key>NSCameraUsageDescription</key>
+<string>This app will use camera.</string>
+<key>NSPhotoLibraryAddUsageDescription</key>
+<string>You can select photos to attach to reports.</string>
+```
+
+### Documentation
+
+Concernant le framework utilisé:
+https://developer.apple.com/documentation/uikit/uiimagepickercontroller
+
+Concernant les liens utiles:
+https://stackoverflow.com/questions/40854886/swift-take-a-photo-and-save-to-photo-library
---
+++ ticket_reader/ticket_reader/ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  ticket_reader
+//
+//  Created by Jeff on 24/12/2017.
+//  Copyright © 2017 Jeff. All rights reserved.
+//
+
+import UIKit
+import CoreImage
+
+class ViewController: UIViewController,UINavigationControllerDelegate, UIImagePickerControllerDelegate  {
+
+    @IBOutlet weak var imageTake: UIImageView!
+    var imagePicker: UIImagePickerController!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func takePhoto(_ sender: UIButton) {
+        imagePicker =  UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        imagePicker.dismiss(animated: true, completion: nil)
+        imageTake.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        let ac = UIAlertController(title: "Saved!", message: "Your altered image has been saved to your photos.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        
+        present(ac, animated: true)
+        
+        var imageToSave: UIImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        UIImageWriteToSavedPhotosAlbum(imageToSave,nil,nil,nil)
+
+
+    }
+/*
+    func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            // we got back an error!
+            let ac = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        } else {
+            let ac = UIAlertController(title: "Saved!", message: "Your altered image has been saved to your photos.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
+    }*/
+
+
+}
+
