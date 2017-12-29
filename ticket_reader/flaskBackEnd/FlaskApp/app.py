0f0a064a92d0617581946cc8ea51399b91ee29f9
Jeff <jeff.chau.qin@gmail.com>
2017-12-29 16:10:20 +0100

    im

---
+++ ticket_reader/flaskBackEnd/FlaskApp/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify, abort, request, make_response, url_for, render_template, send_file
+from PIL import Image
+import base64
+
+
+import json
+
+
+# -------- module app
+app = Flask(__name__)
+
+
+# -------- db load
+db={ 'serie1': 23,'serie2': 22 ,'serie3': 23 ,'serie4': 18 ,'serie5': 23,'id':14 }
+im = Image.open("./static/images/BIG1.jpg")
+encoded_string =  base64.b64encode(open("./static/images/BIG1.jpg", "rb").read())
+
+# -------- db load
+
+@app.route('/')
+def home():
+      return "welcome"
+
+
+@app.route('/<user>')
+def get_tasks(user):
+      return jsonify(encoded_string.decode('utf-8')),200
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
