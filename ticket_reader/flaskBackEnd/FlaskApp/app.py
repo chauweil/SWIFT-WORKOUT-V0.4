dc4cb96be48876d609ad3a3d1983fa8f5c0a3338
Jeff <jeff.chau.qin@gmail.com>
2017-12-31 12:33:57 +0100

    it works

--- ticket_reader/flaskBackEnd/FlaskApp/app.py
+++ ticket_reader/flaskBackEnd/FlaskApp/app.py
@@ -82,7 +82,7 @@ def get_image():
     data2 = np.fromstring(in_memory_file.getvalue(), dtype=np.uint8)
     app.logger.info(bytearray(data2[:4]))
 
-    pickle.dump(content["fileset"],open( "image.jpg", "wb" ))
+    #pickle.dump(content["fileset"],open( "image.jpg", "wb" ))
     return "OK",200
 
 @app.route('/r', methods=["POST"])
