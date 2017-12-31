133673ce3907c3e0081ee5af19a8562117fde6f3
Jeff <jeff.chau.qin@gmail.com>
2017-12-31 18:06:51 +0100

    it works

--- ticket_reader/flaskBackEnd/FlaskApp/app.py
+++ ticket_reader/flaskBackEnd/FlaskApp/app.py
@@ -41,8 +41,8 @@ def get_image2():
     image = io.imread(in_memory_file, as_grey=True)
     atemp = ticket.ticketprocessing(image)
     b = atemp.cropHF(header, footer)
-    io.imsave('./static/images/temp.jpg', b)
-    return send_file("./static/images/temp.jpg", mimetype='image/jpeg')
+    io.imsave('./static/images/temp.jpeg', b)
+    return send_file("./static/images/temp.jpeg", mimetype='image/jpeg')
     #encoded_string = base64.b64encode(open("./static/images/temp.jpg", "rb").read())
     #pickle.dump(content["fileset"],open( "image.jpeg", "wb" ))
 
